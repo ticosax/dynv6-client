ba22670295d9510efb7e44901b0c97c1c33b48da
Nicolas Delaby <ticosax@free.fr>
2016-10-19 14:03:40 +0200

    Set ipv4 to auto

--- client.py
+++ client.py
@@ -13,7 +13,8 @@ import requests
 def main(hostname, token, interface):
     addresses = netifaces.ifaddresses(interface)
     ipv6 = addresses[netifaces.AF_INET6][0]['addr']
-    parameters = {'hostname': hostname, 'token': token, 'ipv6': ipv6}
+    parameters = {'hostname': hostname, 'token': token, 'ipv6': ipv6,
+                  'ipv4': 'auto'}
     response = requests.get('https://dynv6.com/api/update?{}'.format(
         urllib.parse.urlencode(parameters)))
     assert response.status_code == 200, response.content
