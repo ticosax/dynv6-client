7e50ead115bffe24c6a13478a7427d74dab27380
Nicolas Delaby <ticosax@free.fr>
2016-10-21 22:28:51 +0200

    auto doesn't seem to work. switch to api.ipify.org

--- client.py
+++ client.py
@@ -13,8 +13,9 @@ import requests
 def main(hostname, token, interface):
     addresses = netifaces.ifaddresses(interface)
     ipv6 = addresses[netifaces.AF_INET6][0]['addr']
+    ipv4 = requests.get('https://api.ipify.org?format=json').json()['ip']
     parameters = {'hostname': hostname, 'token': token, 'ipv6': ipv6,
-                  'ipv4': 'auto'}
+                  'ipv4': ipv4}
     response = requests.get('https://dynv6.com/api/update?{}'.format(
         urllib.parse.urlencode(parameters)))
     assert response.status_code == 200, response.content
